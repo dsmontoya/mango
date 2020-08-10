21cbd2bf8100e08a38183d45ace8551367f2bd64
Daniel Montoya <dsmontoyam@gmail.com>
2020-08-09 23:02:25 -0500

    omitempty

--- document.go
+++ document.go
@@ -12,7 +12,7 @@ type M bson.M
 
 type Document struct {
 	ID        primitive.ObjectID `bson:"_id" json:"id"`
-	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
-	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
-	DeletedAt *time.Time         `bson:"deletedAt" json:"deletedAt"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt,omitempty"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt,omitempty"`
+	DeletedAt *time.Time         `bson:"deletedAt" json:"deletedAt,omitempty"`
 }
