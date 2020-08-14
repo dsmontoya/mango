95d79fbbfe1d10ef41aa3b66ec307ea4698cc9a9
Daniel Montoya <dsmontoyam@gmail.com>
2020-08-13 23:34:19 -0500

    stage

--- stages/stages.go
+++ stages/stages.go
@@ -13,6 +13,12 @@ func New() Stages {
 	return make(Stages, 0)
 }
 
+//Facet processes multiple aggregation pipelines within a single stage. Each sub-pipeline has its own field in the output document.
+func (s Stages) Facet(pipelines map[string]Stages) Stages {
+	stage := mango.M{"$facet": pipelines}
+	return append(s, stage)
+}
+
 func (s Stages) Match(query operators.Query) Stages {
 	stage := mango.M{"$match": query}
 	return append(s, stage)
--- stages/stages_test.go
+++ stages/stages_test.go
@@ -8,6 +8,42 @@ import (
 	"github.com/dsmontoya/mango/operators"
 )
 
+func TestStages_Facet(t *testing.T) {
+	type args struct {
+		pipelines map[string]Stages
+	}
+	tests := []struct {
+		name string
+		s    Stages
+		args args
+		want Stages
+	}{
+		{
+			"sample",
+			New(),
+			args{
+				map[string]Stages{
+					"field": New().Sample(3),
+				},
+			},
+			Stages{
+				{
+					"$facet": map[string]Stages{
+						"field": New().Sample(3),
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Facet(tt.args.pipelines); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Stages.Facet() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestStages_Match(t *testing.T) {
 	type args struct {
 		query operators.Query
